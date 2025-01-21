45ff84be89180d94d15a79d13232030949b35965
Muhammad Naeem Tahir <naeem.tahir@funavry.com>
2025-01-21 12:53:13 +0500

    await for post data

--- src/app/posts/[postID]/page.tsx
+++ src/app/posts/[postID]/page.tsx
@@ -1,5 +1,5 @@
 export const runtime = "edge";
-// import axios from "axios";
+import axios from "axios";
 
 type Post = {
     id: number;
@@ -13,14 +13,20 @@ const URL = "https://jsonplaceholder.typicode.com/posts";
 export default async function PostDetails({ params }: { params: Promise<{ postID: string }> }) {
     const { postID } = await params;
 
-    // const response = await axios.get<Post>(`${URL}/${postID}`);
-    // const post = await response.data;
+    const response = await axios.get<Post>(`${URL}/${postID}`);
+    const post = await response.data;
 
     return (
         <main className="container mx-auto my-5">
-            <h2 className="text-2xl text-center">{postID}</h2>
-            {/* <h2 className="text-2xl text-center">{post.title}</h2> */}
-            {/* <p>{post.body}</p> */}
+            {
+                post && (
+                    <>
+                        <h2 className="text-2xl text-center">{postID}</h2>
+                        <h2 className="text-2xl text-center">{post.title}</h2>
+                        <p>{post.body}</p>
+                    </>
+                )
+            }
         </main>
     );
 }
